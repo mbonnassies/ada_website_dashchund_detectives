b3f593883608b7035e73da55b66b8777c828300d
SSamoht <samohtblanchard@gmail.com>
2023-12-22 17:12:05 +0100

    Added Barbie/OH

--- assets/js/plots/rating_vs_extro.js
+++ assets/js/plots/rating_vs_extro.js
@@ -1,21 +1,26 @@
 // set the dimensions and margins of the graph
-var margin3 = {top: 30, right: 30, bottom: 50, left: 60},
-    width3 = 800 - margin3.left - margin3.right,
-    height = 500 - margin3.top - margin3.bottom;
+var margin8 = {top: 30, right: 30, bottom: 50, left: 60},
+    width3 = 800 - margin8.left - margin8.right,
+    height = 500 - margin8.top - margin8.bottom;
 
 var formatNumber4 = d3.format(".2f");
 
 // append the svg object to the body of the page
 var svg10 = d3.select("#ratings_vs_extro")
   .append("svg")
-    .attr("width", width3 + margin3.left + margin3.right)
-    .attr("height", height + margin3.top + margin3.bottom)
+    .attr("width", width3 + margin8.left + margin8.right)
+    .attr("height", height + margin8.top + margin8.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + margin3.left + "," + margin3.top + ")");
+          "translate(" + margin8.left + "," + margin8.top + ")");
 
 //Read the data
 d3.csv("data/extro.csv", function(data) {
+
+  var dataPoints = [
+    {extro_proportion: 8/16, averageRating: 7, color: "#b22178", name:'Barbie'},
+    {extro_proportion: 7/19, averageRating: 8.4, color: "#1b1b1b", name: 'Oppenheimer'}
+  ];
 
   // Add X axis
   var x = d3.scaleLinear()
@@ -73,17 +78,37 @@ d3.csv("data/extro.csv", function(data) {
     .on('mouseover', tip.show)  // Show tooltip on mouseover
     .on('mouseout', tip.hide);  // Hide tooltip on mouseout
 
+  dataPoints.forEach(function(d) {
+    svg10.append("circle")
+      .attr("cx", x(d.extro_proportion))
+      .attr("cy", y(d.averageRating))
+      .attr("r", 3)
+      .style("fill", d.color)
+      .style("opacity", 1)
+      .style("stroke", "white")
+      .on('mouseover', tip.show)  // Show tooltip on mouseover
+      .on('mouseout', tip.hide);  // Hide tooltip on mouseout
+    svg10.append("text")
+      .attr("x", x(d.extro_proportion))
+      .attr("y", y(d.averageRating) - 12)
+      .text(d.name)  // Set the text to the averageRating value
+      .attr("font-size", "10px")
+      .attr("dx", "-.8em")
+      .attr("dy", ".35em")
+      .style("fill", "black");
+  });
+
   // Chart title
   svg10.append("text")
     .attr("x", width3 / 2)
-    .attr("y", 0 - (margin3.top / 2))
+    .attr("y", 0 - (margin8.top / 2))
     .attr("text-anchor", "middle")
     .style("font-size", "16px")
     .text("Average Rating vs Extroverted Proportion");
 
   svg10.append("text")
     .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin3.left)
+    .attr("y", 0 - margin8.left)
     .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
     .style("text-anchor", "middle")
@@ -92,7 +117,7 @@ d3.csv("data/extro.csv", function(data) {
 
   svg10.append("text")
     .attr("x", (width3 / 2))
-    .attr("y", height + margin3.bottom)
+    .attr("y", height + margin8.bottom)
     .attr("text-anchor", "middle")
     .style("font-size", "12px")
     .text("Extroverted Proportion");
